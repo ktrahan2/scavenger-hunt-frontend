e79b1f1e132be7f3f64d79f2d8ecf6fbffc2d85f
Kyle Trahan <ktrahan2@gmail.com>
2020-11-05 19:25:01 -0500

    photo working on createdHunt

--- CreatedHunt.js
+++ CreatedHunt.js
@@ -1,38 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Button, StyleSheet, Text, View, Image } from 'react-native';
-import { CheckBox } from 'react-native-elements'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { 
     faChevronCircleDown
   } from '@fortawesome/free-solid-svg-icons'
 
-
 function CreatedHunt({
     isThemeArray,
-    isChecked,
     isItemClicked,
     clickItem,
     unClickItem,
-    check,
-    uncheck
-
     }) {
 
     const handleClick = ( item ) => {
         if (isItemClicked !== item.name) {
+            console.log("clicked")
             clickItem(item.name)
         } else {
             unClickItem(item.name)
-        }
-    }
-        
-    const handleCheck = ( event, item ) => {
-        event.preventDefault()
-        if (isChecked.includes(item.name)) {
-            uncheck(item.name)
-        } else {
-            check(item.name)
         }
     }
     
@@ -40,17 +26,12 @@ function CreatedHunt({
         return isThemeArray.map(item => {
             return (
                 <View style={styles.listItem} key={item.ID}>
-                <CheckBox
-                    checked={isChecked.includes(item.name) ? true : false}
-                    onPress={(event) => handleCheck(event, item)}
-                    containerStyle={styles.checkbox}
-                />
-                <Text 
-                    style={styles.text}
-                    onPress={() => handleClick(item)}
-                    >
-                    {item.name} <FontAwesomeIcon icon={ faChevronCircleDown } />
-                </Text>
+                    <Text 
+                        style={styles.text}
+                        onPress={() => handleClick(item)}
+                        >
+                        {item.name} <FontAwesomeIcon icon={ faChevronCircleDown } />
+                    </Text>
                 <View>
                     { isItemClicked === item.name ?
                         <Image
@@ -89,15 +70,7 @@ function mapDispatchToProps(dispatch) {
         unClickItem: () => dispatch({
             type: "UNCLICKED",
             payload: ""
-        }),
-        check: (item) => dispatch({
-            type: "CHECK",
-            payload: item
-        }),
-        uncheck: (item) => dispatch({
-            type: "UNCHECK",
-            payload: item
-        })      
+        }),     
     }
   
 }
@@ -107,5 +80,12 @@ export default connect(mapStateToProps, mapDispatchToProps)(CreatedHunt);
 const styles = StyleSheet.create({
     screenContainer: {
         flex: 1
-    }
+    },
+    itemImage: {
+        borderWidth: 3,
+        borderRadius: 10,
+        borderColor: 'orange',
+        width: 200,
+        height: 200,
+      },
 })
